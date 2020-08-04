9f8c9f4ea3b832ee7eea81db03f556e8a581f0c1
Jasmine Terry <jasmine-terry91@gmail.com>
2020-08-03 22:25:51 -0400

    songs added, play on click, play icon added, visualizer working

---
+++ app.js
@@ -0,0 +1,32 @@
+import AudioPlayer from './Player.js';
+
+const player = new AudioPlayer('.audioPlayer', [
+   { name: 'Hello', url: 'songs/01 Hello.mp3'}, 
+   {url: 'songs/12 Latch (Acoustic).mp3', name: 'Latch'},
+   {url: 'songs/15 Sunday Candy.mp3', name: 'Sunday Candy'},
+   {url: 'songs/02 Brand New (Duet with Justin Timbe.mp3', name: 'Brand New'},
+   {url: 'songs/02 Im a Mess.mp3', name: 'Im a Mess'},
+   {url: 'songs/02 Send My Love (To Your New Lover).mp3', name: 'Send My Love( To Your New Lover)'},
+   {url: 'songs/03 Collard Greens (feat. Kendrick La.mp3', name: 'Collard Greens'},
+   {url: 'songs/03 I Miss You.mp3', name: 'I Miss You'},
+   {url: 'songs/03 Sing.mp3', name: 'Sing'},
+   {url: 'songs/03 Warm Enough.mp3', name: 'Warm Enough'},
+   {url: 'songs/04 Dont.mp3', name: 'Dont'},
+   {url: 'songs/04 Nothing Came to Me.mp3', name: 'Nothing Came To Me'},
+   {url: 'songs/05 Happy (From _Despicable Me 2_).mp3', name: 'Happy'},
+   {url: 'songs/04 When We Were Young.mp3', name: 'When We Were Young'},
+   {url: 'songs/05 Remedy.mp3', name: 'Remedy'},
+   {url: 'songs/05 Wanna Be Cool.mp3', name: 'Wanna Be Cool'},
+   {url: 'songs/06 Water Under the Bridge.mp3', name: 'Water Under The Bridge'},
+   {url: 'songs/07 Caretaker.mp3', name: 'Caretaker'},
+   {url: 'songs/07 River Lea.mp3', name: 'River Lea'},
+   {url: 'songs/08 Lost Queen.mp3', name: 'Lost Queen'},
+   {url: 'songs/08 Love in the Dark.mp3', name: 'Love In The Dark'},
+   {url: 'songs/09 Know Who You Are (Duet with Alici.mp3', name: 'Know Who You Are'},
+   {url: 'songs/10 Hell of a Night.mp3', name: 'Hell Of A Night'},
+   {url: 'songs/11 Thinking Out Loud.mp3', name: 'Thinking Out Loud'},
+   {url: 'songs/12 Afire Love.mp3', name :'Afire Love'},
+
+]
+
+)
